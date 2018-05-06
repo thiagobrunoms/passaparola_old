2d0f4253bb7c1604f0d15ad28c7e20bb33f6d431
Thiago Sales <thiago@quantogastei.com>
2018-05-06 18:22:25 -0300

    Adds listeners for new parolas

--- app/src/main/java/com/passaparola/thiagodesales/passaparolaview/listeners/NewParolaListener.java
+++ app/src/main/java/com/passaparola/thiagodesales/passaparolaview/listeners/NewParolaListener.java
@@ -3,7 +3,7 @@ package com.passaparola.thiagodesales.passaparolaview.listeners;
 import com.passaparola.thiagodesales.passaparolaview.model.Parola;
 
 
-public interface OnNewParolaListener {
+public interface NewParolaListener {
 
     public void onNewParola(Parola parola);
 
